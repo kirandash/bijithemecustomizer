d9e529ad0ece06578759b4ab73f2fa9151c38311
kiran kumar dash <thekirankumardash@gmail.com>
2015-08-16 00:58:34 +0530

    Add functions for static front page sections and controls.

--- wp-content/themes/bijithemecustomizer/functions.php
+++ wp-content/themes/bijithemecustomizer/functions.php
@@ -2,7 +2,7 @@
 
 function wpt_register_theme_customizer( $wp_customize ) {
 
-	//var_dump( $wp_customize -> controls() );
+	//var_dump( $wp_customize );
 
 	// Customize title and tagline sections and labels
 	$wp_customize->get_section('title_tagline')->title = __('Site Name and Description', 'wptthemecustomizer');  
@@ -11,6 +11,12 @@ function wpt_register_theme_customizer( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 
+	// Customize the Front Page Settings
+	$wp_customize->get_section('static_front_page')->title = __('Homepage Preferences', 'wptthemecustomizer');
+	$wp_customize->get_section('static_front_page')->priority = 20;
+	$wp_customize->get_control('show_on_front')->label = __('Choose Homepage Preference', 'wptthemecustomizer');  
+	$wp_customize->get_control('page_on_front')->label = __('Select Homepage', 'wptthemecustomizer');  
+	$wp_customize->get_control('page_for_posts')->label = __('Select Blog Homepage', 'wptthemecustomizer');  
 
 }
 add_action( 'customize_register', 'wpt_register_theme_customizer' );
